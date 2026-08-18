0e17c97b19518f3ad000170a9ecd9eff2121f722
Matheus Viuge <matheus.viuge05@gmail.com>
2026-08-18 18:19:59 -0300

    ci: sync protected route helper use (#21)

--- frontend/src/pages/Layout/PrivateRoute.tsx
+++ frontend/src/pages/Layout/PrivateRoute.tsx
@@ -2,8 +2,7 @@ import { useUser } from "@/hooks/useUser";
 import { Navigate, useLocation } from "react-router-dom";
 import { Layout } from "./Layout";
 import { Loader } from "./Loader";
-
-const protectedBasePaths = ["/instrutor", "/aluno", "/admin"] as const;
+import { protectedBasePaths } from "./protectedRoutes";
 
 export function PrivateRoute() {
     const { user, loading, isAuthenticated } = useUser();
