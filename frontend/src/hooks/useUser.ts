558d64ee4ea90280a89b40f2f5f69ab6b267d069
Matheus Viuge <matheus.viuge05@gmail.com>
2026-08-18 18:09:55 -0300

    refactor: import user context from definition module (#5)

--- frontend/src/hooks/useUser.ts
+++ frontend/src/hooks/useUser.ts
@@ -1,12 +1,12 @@
-import { UserContext } from "@/Context/UserContext";
 import { useContext } from "react";
+import { UserContext } from "@/Context/userContextDefinition";
 
 export function useUser() {
   const context = useContext(UserContext);
-  
+
   if (!context) {
-    throw new Error('useUser deve ser usado dentro de UserProvider');
+    throw new Error("useUser deve ser usado dentro de UserProvider");
   }
-  
+
   return context;
 }
